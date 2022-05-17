7d87de82001f59162e604d2bbb24079447a26bf7
tominaga <aurora25sh.50nonono@gmail.com>
2022-05-17 22:29:55 +0900

    不要な配列を削除

--- 01.fizzbuzz/fizzbuzz.js
+++ 01.fizzbuzz/fizzbuzz.js
@@ -1,13 +1,11 @@
-let numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20];
-
-for (let i = 0; i < 20; i ++) {
-  if (numbers[i] % 15 === 0) {
+for (let i = 0; i <= 20; i++) {
+  if (i % 15 === 0) {
     console.log('FizzBuzz');
-  } else if (numbers[i] % 3 === 0) {
+  } else if (i % 3 === 0) {
     console.log('Fizz');
-  } else if (numbers[i] % 5 === 0) {
+  } else if (i % 5 === 0) {
     console.log('Buzz');
   } else {
-    console.log(numbers[i]);
+    console.log(i);
   }
 };
