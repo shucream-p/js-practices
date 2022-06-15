9c842afa075791fce8be2efcdc2ce83b9e1cdd6c
tominaga <aurora25sh.50nonono@gmail.com>
2022-06-15 14:46:00 +0900

    標準入力をデータベースに保存する処理を記述

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,3 +1,6 @@
+const sqlite3 = require('sqlite3').verbose()
+const db = new sqlite3.Database('memo.sqlite')
+
 process.stdin.setEncoding('utf8')
 
 const lines = []
@@ -10,5 +13,14 @@ rl.on('line', (line) => {
   lines.push(line)
 })
 rl.on('close', () => {
-  console.log(lines)
+  db.serialize(() => {
+    db.run('CREATE TABLE IF NOT EXISTS memos (title TEXT, content TEXT)')
+    const title = lines[0]
+    lines.shift()
+    const content = lines.join('\n')
+    const insertData = db.prepare('INSERT INTO memos VALUES (?, ?)')
+    insertData.run(title, content)
+    insertData.finalize()
+  })
+  db.close()
 })
