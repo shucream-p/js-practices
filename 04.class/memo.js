9403ec4b48496fa1c14b711faa1e3871eed24ade
tominaga <aurora25sh.50nonono@gmail.com>
2022-06-17 18:37:10 +0900

    非同期処理をasync/awaitで記述

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -2,90 +2,72 @@ const sqlite3 = require('sqlite3').verbose()
 const db = new sqlite3.Database('memo.sqlite')
 const options = require('minimist')(process.argv.slice(2))
 
-if (options.l) {
-  db.all('SELECT title FROM memos', (err, rows) => {
-    if (err) {
-      console.error(err.message)
-    }
-    rows.forEach(row => {
-      console.log(row.title)
+function getMemoTitles () {
+  return new Promise(resolve => {
+    const memoTitles = []
+    db.all('SELECT title FROM memos', (err, rows) => {
+      if (err) {
+        console.error(err.message)
+      }
+      rows.forEach(row => {
+        memoTitles.push(row.title)
+      })
+      resolve(memoTitles)
     })
   })
-  db.close()
-} else if (options.r) {
-  (() => {
-    return new Promise(resolve => {
-      const memoTitles = []
-      db.all('SELECT title FROM memos', (err, rows) => {
-        if (err) {
-          console.error(err.message)
-        }
-        rows.forEach(row => {
-          memoTitles.push(row.title)
-        })
-        resolve(memoTitles)
-      })
-    })
-  })().then(memoTitles => {
-    const { Select } = require('enquirer')
+}
 
+if (options.l) {
+  (async () => {
+    const memoTitles = await getMemoTitles()
+    memoTitles.forEach(title => {
+      console.log(title)
+    })
+    db.close()
+  })()
+} else if (options.r) {
+  (async () => {
+    const memoTitles = await getMemoTitles()
+    const { Select } = require('enquirer')
     const prompt = new Select({
       name: 'title',
       message: 'Choose a note you want to see:',
       choices: memoTitles
     })
+    const answer = await prompt.run()
 
-    prompt.run()
-      .then(title => {
-        db.get(`SELECT content FROM memos WHERE title = '${title}'`, (err, row) => {
-          if (err) {
-            console.error(err.message)
-          }
-          console.log(row.content)
-        })
-        db.close()
-      })
-  })
-} else if (options.d) {
-  (() => {
-    return new Promise(resolve => {
-      const memoTitles = []
-      db.all('SELECT title FROM memos', (err, rows) => {
-        if (err) {
-          console.error(err.message)
-        }
-        rows.forEach(row => {
-          memoTitles.push(row.title)
-        })
-        resolve(memoTitles)
-      })
+    db.get(`SELECT content FROM memos WHERE title = '${answer}'`, (err, row) => {
+      if (err) {
+        console.error(err.message)
+      }
+      console.log(row.content)
     })
-  })().then(memoTitles => {
+    db.close()
+  })()
+} else if (options.d) {
+  (async () => {
+    const memoTitles = await getMemoTitles()
     const { Select } = require('enquirer')
-
     const prompt = new Select({
       name: 'title',
       message: 'Choose a note you want to delete:',
       choices: memoTitles
     })
+    const answer = await prompt.run()
 
-    prompt.run()
-      .then(title => {
-        db.get(`DELETE FROM memos WHERE title = '${title}'`, err => {
-          if (err) {
-            console.error(err.message)
-          }
-        })
-        db.close()
-      })
-  })
+    db.get(`DELETE FROM memos WHERE title = '${answer}'`, err => {
+      if (err) {
+        console.error(err.message)
+      }
+    })
+    db.close()
+  })()
 } else {
   process.stdin.setEncoding('utf8')
 
   const lines = []
   const rl = require('readline').createInterface({
-    input: process.stdin,
-    output: process.stdout
+    input: process.stdin
   })
 
   rl.on('line', (line) => {
