d6390b2a88bb64d9ae2cd056c42c6f0fe74a72e1
tominaga <aurora25sh.50nonono@gmail.com>
2022-06-22 16:26:42 +0900

    StorageクラスとMemoControllerクラスを作成

--- 04.class/memo.js
+++ 04.class/memo.js
@@ -1,69 +1,89 @@
-const sqlite3 = require('sqlite3').verbose()
-const db = new sqlite3.Database('memo.sqlite')
 const options = require('minimist')(process.argv.slice(2))
 
-function getMemoTitles () {
-  return new Promise(resolve => {
-    const memoTitles = []
-    db.all('SELECT title FROM memos', (err, rows) => {
-      if (err) {
-        console.error(err.message)
-      }
-      rows.forEach(row => {
-        memoTitles.push(row.title)
+class Storage {
+  constructor () {
+    this.sqlite3 = require('sqlite3').verbose()
+    this.db = new this.sqlite3.Database('memo.sqlite')
+  }
+
+  create (texts) {
+    // データベースにメモを保存
+    this.db.serialize(() => {
+      this.db.run('CREATE TABLE IF NOT EXISTS memos (title TEXT, content TEXT)')
+      const title = texts[0]
+      texts.shift()
+      const content = texts.join('\n')
+      const insertData = this.db.prepare('INSERT INTO memos VALUES (?, ?)')
+      insertData.run(title, content)
+      insertData.finalize()
+    })
+  }
+
+  read () {
+    // データベースからメモを読み込む
+    return new Promise(resolve => {
+      const memos = []
+      this.db.all('SELECT * FROM memos', (err, rows) => {
+        if (err) {
+          console.error(err.message)
+        }
+        rows.forEach(row => {
+          memos.push(row)
+        })
+        resolve(memos)
       })
-      resolve(memoTitles)
     })
-  })
-}
+  }
 
-async function generatePrompt (message) {
-  const memoTitles = await getMemoTitles()
-  const { Select } = require('enquirer')
-  const prompt = new Select({
-    name: 'title',
-    message: `Choose a note you want to ${message}:`,
-    choices: memoTitles
-  })
-
-  return prompt
-}
-
-if (options.l) {
-  (async () => {
-    const memoTitles = await getMemoTitles()
-    memoTitles.forEach(title => {
-      console.log(title)
-    })
-    db.close()
-  })()
-} else if (options.r) {
-  (async () => {
-    const prompt = await generatePrompt('see')
-    const answer = await prompt.run()
-
-    db.get(`SELECT content FROM memos WHERE title = '${answer}'`, (err, row) => {
-      if (err) {
-        console.error(err.message)
-      }
-      console.log(row.content)
-    })
-    db.close()
-  })()
-} else if (options.d) {
-  (async () => {
-    const prompt = await generatePrompt('delete')
-    const answer = await prompt.run()
-
-    db.get(`DELETE FROM memos WHERE title = '${answer}'`, err => {
+  delete (answer) {
+    // データベースからメモを削除
+    this.db.run(`DELETE FROM memos WHERE title = '${answer}'`, err => {
       if (err) {
         console.error(err.message)
       }
     })
-    db.close()
-  })()
-} else {
-  function getText () {
+  }
+}
+
+class MemoController {
+  constructor () {
+    this.storage = new Storage()
+  }
+
+  async create () {
+    const texts = await this.getStdin()
+    this.storage.create(texts)
+  }
+
+  async index () {
+    const memos = await this.storage.read()
+    memos.forEach(memo => {
+      console.log(memo.title)
+    })
+  }
+
+  async show () {
+    const memos = await this.storage.read()
+    const titles = memos.map(memo => {
+      return memo.title
+    })
+    const prompt = await this.generatePrompt('see', titles)
+    const answer = await prompt.run()
+    const memo = memos.find(memo => memo.title === answer)
+    console.log(memo.content)
+  }
+
+  async delete () {
+    const memos = await this.storage.read()
+    const titles = memos.map(memo => {
+      return memo.title
+    })
+    const prompt = await this.generatePrompt('delete', titles)
+    const answer = await prompt.run()
+    this.storage.delete(answer)
+  }
+
+  getStdin () {
     return new Promise(resolve => {
       process.stdin.setEncoding('utf8')
       const lines = []
@@ -80,17 +100,36 @@ if (options.l) {
     })
   }
 
-  (async () => {
-    const texts = await getText()
-    db.serialize(() => {
-      db.run('CREATE TABLE IF NOT EXISTS memos (title TEXT, content TEXT)')
-      const title = texts[0]
-      texts.shift()
-      const content = texts.join('\n')
-      const insertData = db.prepare('INSERT INTO memos VALUES (?, ?)')
-      insertData.run(title, content)
-      insertData.finalize()
+  generatePrompt (message, titles) {
+    return new Promise(resolve => {
+      const { Select } = require('enquirer')
+      const prompt = new Select({
+        name: 'title',
+        message: `Choose a note you want to ${message}:`,
+        choices: titles
+      })
+
+      resolve(prompt)
     })
-    db.close()
-  })()
+  }
 }
+
+function main () {
+  const memoController = new MemoController()
+
+  if (options.l) {
+    // タイトル一覧を表示する処理
+    memoController.index()
+  } else if (options.r) {
+    // タイトル一覧から選択してメモを参照する処理
+    memoController.show()
+  } else if (options.d) {
+    // タイトル一覧から選択してメモを削除する処理
+    memoController.delete()
+  } else {
+    // 標準入力を受け取って、メモを保存する処理
+    memoController.create()
+  }
+}
+
+main()
